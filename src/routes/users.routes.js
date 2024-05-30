ebeb3e0c8c8058b19b98ba8a16fe9534bafd3786
Natalia Kals <nataliakals03@gmail.com>
2024-05-31 08:53:33 +1200

    creating a DiskStorage

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,18 @@
-const { Router } = require('express');
-const usersRoutes = Router();
+const { Router, response } = require('express');
 const UserController = require('../controllers/UsersController');
 const ensureAuthenticated = require("../middleware/ensureAuthenticated");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
+const usersRoutes = Router();
 const userController = new UserController();
+const upload = multer(uploadConfig.MULTER);
+
 usersRoutes.post('/', userController.create);
 usersRoutes.put('/', ensureAuthenticated, userController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), (request, response) => {
+    response.json();
+})
+
 
 module.exports = usersRoutes;
